fdf5f2ed248e440353ffd815c75e35c4b5cc50ab
dima9917 <41657345+dima9917@users.noreply.github.com>
2023-03-21 16:44:38 -0700

    print messages in cyan

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine($"I'm printing in {Console.ForegroundColor.ToString()}");
+Console.ResetColor();
